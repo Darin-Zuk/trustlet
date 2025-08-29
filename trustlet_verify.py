41b837b302d8e82acdc6f7b87ea79d5de339dc8f
Darin Zukerman <zukermandarin@gmail.com>
2025-08-29 08:48:48 +0200

    add_verify_app

---
+++ trustlet_verify.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Trustlet - Email Verified")
+
+st.title("✅ Email Verified")
+
+st.success(
+    "Thank you for verifying your email.\n\n"
+    "You will receive another email when your nominated existed user approves your application. "
+    "After that, you’ll be able to log in to Trustlet."
+)
