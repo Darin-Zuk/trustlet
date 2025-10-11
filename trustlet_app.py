e3a32b934da1654b9db6785e108c4d76ae0f1d7a
Darin Zukerman <zukermandarin@gmail.com>
2025-10-11 10:28:53 +0200

    Update trustlet_app.py

--- trustlet_app.py
+++ trustlet_app.py
@@ -647,8 +647,8 @@ else:
             }).execute()
             st.success("Listing added!")
 
-    if res.data:
-        notify_matching_alerts_for_listing(res.data[0])    
+            if res.data:
+                notify_matching_alerts_for_listing(res.data[0])
 
         st.markdown("---")
         st.subheader("Your listings (activate/deactivate)")
